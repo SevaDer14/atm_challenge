4400811f30f769191b133216df4e257e9867e533
kim_haaga <kim.haaga@live.se>
2021-03-30 14:21:21 +0200

    Add expiration date test and case

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,7 +2,7 @@ require './lib/atm.rb'
 require 'date'
 
 describe Atm do 
-  let(:account) { instance_double('Account', pin_code: '1234') }
+  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '03/22') }
 
   before do
     # Before each test we need to add an attribute of 'balance'
@@ -47,6 +47,12 @@ describe Atm do
   it 'rejects withdraw if the PIN is wrong' do
     expected_output = { status:false, message: 'wrong PIN', date: Date.today }
     expect(subject.withdraw(50, 9999, account)).to eq expected_output
+  end
+
+  it 'reject withdraw if the card is expired' do
+    allow(account).to receive(:exp_date).and_return('12/15')
+    expected_output = { status: false, message: 'card expired', date: Date.today }
+    expect(subject.withdraw(6, '1234', account)).to eq expected_output
   end
 
 end
