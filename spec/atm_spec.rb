9b50f046399da0d7d8dbb02c516913b0cd408369
kim_haaga <kim.haaga@live.se>
2021-03-30 21:03:12 +0200

    Add bill counting method and test

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -25,9 +25,7 @@ describe Atm do
   it 'allow withdraw if the account has enough balance.' do
     # Creating a variable for the 'expected output' after
     # successful withdraw
-
-    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
-
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 45, bills: [20, 20, 5]}
     # Passing in withdraw amount and account
     expect(subject.withdraw(45, '1234', account)).to eq expected_output
   end
