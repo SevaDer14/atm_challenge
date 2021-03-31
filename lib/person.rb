9186a05b5490f97348eed481253f705e3c7875c5
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-31 20:09:55 +0200

    Updates Person class: expands set_name method adds error handler for nil @name at initialize

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,20 @@
 require 'pry'
 
 class Person
-  attr_accessor :name
+attr_accessor :name
 
-  def initialize(attrs) 
-    @name = attrs
-  end
+def initialize(attrs = {}) 
+set_name(attrs[:name])
+end
+
+def set_name(obj)
+obj == nil ? missing_name : @name = obj
+end
+
+private
+
+def missing_name
+raise 'A name is required'
+end
+
 end
