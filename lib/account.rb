dc066bc70040db52bdd4a6fa46081558eb35c9e0
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-31 09:52:45 +0200

    Add Account class with PIN and exp.date initialized and tests

---
+++ lib/account.rb
@@ -0,0 +1,19 @@
+require "pry"
+
+class Account
+    STANDARD_VALIDITY_YRS = 5
+    attr_accessor :pin_code
+    attr_accessor :exp_date
+    
+    #ICOMPLETE: add possibility for 0000 PIN generation 
+    def initialize
+        @pin_code = rand(1000..9999)
+        @exp_date = set_expire_date
+        #@account_status: :active
+    end
+
+    def set_expire_date
+        Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    end
+    
+end
