c40dd7d7295a5da7d916c31f90b8702bdf0e5533
kim_haaga <kim.haaga@live.se>
2021-03-31 12:52:09 +0200

    Update Account class, iniialize method @account_status

--- lib/account.rb
+++ lib/account.rb
@@ -4,12 +4,14 @@ class Account
     STANDARD_VALIDITY_YRS = 5
     attr_accessor :pin_code
     attr_accessor :exp_date
+    attr_accessor :account_status
+    
     
     #ICOMPLETE: add possibility for 0000 PIN generation 
     def initialize
         @pin_code = rand(1000..9999)
         @exp_date = set_expire_date
-        #@account_status: :active
+        @account_status = :active
     end
 
     def set_expire_date
