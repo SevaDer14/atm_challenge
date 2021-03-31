17d2ece79f8bd1a1a4120d09784e316ba549183a
kim_haaga <kim.haaga@live.se>
2021-03-31 20:19:04 +0200

    Add initialize @cash

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,23 @@
 require 'pry'
 
 class Person
-attr_accessor :name
+  attr_accessor :name, :cash
 
-def initialize(attrs = {}) 
-set_name(attrs[:name])
-end
+  def initialize(attrs = {}) 
+    set_name(attrs[:name])
+    @cash = 0
+  end
+  
+  def set_name(obj)
+    obj == nil ? missing_name : @name = obj
+  end
 
-def set_name(obj)
-obj == nil ? missing_name : @name = obj
-end
 
-private
-
-def missing_name
-raise 'A name is required'
-end
+  
+  private
+  
+  def missing_name
+    raise 'A name is required'
+  end
 
 end
