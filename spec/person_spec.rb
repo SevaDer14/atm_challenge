3b89f284b48feb549c947a9ffd7d7a627d0e9cdf
kim_haaga <kim.haaga@live.se>
2021-03-31 20:56:22 +0200

    Add owner class test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -29,7 +29,8 @@ describe Person do
       expect(subject.account).to be_an_instance_of Account
     end
 
+    it 'with himself as an owner' do
+      expect(subject.account.owner).to be subject
+    end
   end
-
-
 end
