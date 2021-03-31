ff0d17ed847275bf6eebc8e0879c4068d40f2458
sevader14 <vsevolod.deriushkin@gmail.com>
2021-03-31 13:58:18 +0200

    Update Account Class Initialaze method with @owner new private method to set the @owner new private method to raise error if @owner = nil

--- lib/account.rb
+++ lib/account.rb
@@ -5,13 +5,17 @@ class Account
     attr_accessor :pin_code
     attr_accessor :exp_date
     attr_accessor :account_status
+    attr_accessor :owner
     
     
     #ICOMPLETE: add possibility for 0000 PIN generation 
-    def initialize
+    def initialize(attrs = {})
         @pin_code = rand(1000..9999)
         @exp_date = set_expire_date
         @account_status = :active
+               
+        set_owner(attrs[:owner])
+        
     end
 
     def set_expire_date
@@ -22,4 +26,14 @@ class Account
         @account_status = :deactivated
     end
     
+    private
+
+    def set_owner(obj)        
+        obj == nil ? missing_owner : @owner = obj
+    end
+
+    def missing_owner        
+        raise "An Account owner is required"
+    end    
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,9 @@ require "date"
 
 describe Account do
 
+    let(:person) {instance_double('Person', name: 'Bob')}
+    subject { described_class.new({owner: person}) }
+
     it 'check the length of a number' do
         number = 1234
         number_length = Math.log10(number).to_i + 1
@@ -21,6 +24,15 @@ describe Account do
     it 'deactivates account using the instance method' do 
         subject.deactivate
         expect(subject.account_status).to eq :deactivated
+    end
+
+    it 'is expected to have an owner' do        
+        #subject({owner: person})
+        expect(subject.owner).to eq person
+    end
+
+    it 'is expected to raise error if no owner is set' do
+        expect{ described_class.new }.to raise_error "An Account owner is required"
     end
 
 end
